c0d0d98ec6d325711c431354429435bb2ceeef0f
spankova <dshpankova282002@gmail.com>
2022-07-10 21:13:37 +0300

    feat(main.ts): created server

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = await app.get(ConfigService);
+  const PORT = config.get<number>('PORT');
+  const HOST = config.get<string>('HOST');
+
+  await app.listen(PORT, HOST);
+
+  Logger.log(`Server started on ${await app.getUrl()}`);
+}
+bootstrap();
