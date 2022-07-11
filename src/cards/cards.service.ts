89f495f5a240f2bc07250d1ed9eaa97feaf1103d
spankova <dshpankova282002@gmail.com>
2022-07-11 12:47:49 +0300

    feat: created cards service and function for creating cards

---
+++ src/cards/cards.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CardEntity } from './entities/card.entity';
+import { CreateCardInput } from './inputs/create-cards.input';
+
+@Injectable()
+export class CardsService {
+  constructor(
+    @InjectRepository(CardEntity)
+    private readonly cardRepository: Repository<CardEntity>,
+  ) {}
+
+  async createCard(createCardInput: CreateCardInput): Promise<CardEntity> {
+    const card = await this.cardRepository.save({ ...createCardInput });
+    return card;
+  }
+}
