4e120da37c8af4a2ff3f8d6f69fa8425c6b35dee
spankova <dshpankova282002@gmail.com>
2022-07-11 13:01:06 +0300

    feat: added get method to search card by id

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CreateCardInput } from './inputs/create-cards.input';
 
@@ -9,5 +9,10 @@ export class CardsController {
   @Post()
   create(@Body() cardInput: CreateCardInput) {
     return this.cardService.createCard(cardInput);
+  }
+
+  @Get(':card_id')
+  findById(@Param('card_id') card_id: number) {
+    return this.cardService.findCardById(card_id);
   }
 }
