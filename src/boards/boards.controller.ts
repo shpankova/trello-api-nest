e3a152b23401c8488e012855bd1cdb79a1437b16
spankova <dshpankova282002@gmail.com>
2022-07-12 00:06:45 +0300

    feat(board.controller): added error handling

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,16 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
+  Next,
   Param,
   Post,
   Put,
+  Res,
 } from '@nestjs/common';
+import { Response, NextFunction } from 'express';
 import { BoardsService } from './boards.service';
 import { CreateBoardInput } from './inputs/create-boards.input';
 import { UpdateBoardInput } from './inputs/update-board.input';
@@ -16,8 +20,24 @@ export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
 
   @Post()
-  create(@Body() boardInput: CreateBoardInput) {
-    return this.boardsService.createBoard(boardInput);
+  async create(
+    @Body() boardInput: CreateBoardInput,
+    @Res() res: Response,
+    @Next() next: NextFunction,
+  ) {
+    try {
+      const board = await this.boardsService.createBoard(boardInput);
+      res.json({
+        message: 'Board added successfully!',
+        body: { board },
+      });
+    } catch (err) {
+      if (err.message === 'Board exists') {
+        throw new BadRequestException('This board already exists');
+      } else {
+        next(err);
+      }
+    }
   }
 
   @Get()
@@ -25,18 +45,59 @@ export class BoardsController {
     return this.boardsService.findAllBoards();
   }
 
-  @Get(':board_id')
-  getById(@Param('board_id') board_id: number) {
-    return this.boardsService.findBoardById(board_id);
+  @Get(':id')
+  async getById(
+    @Param('id') id: number,
+    @Res() res: Response,
+    @Next() next: NextFunction,
+  ) {
+    try {
+      const board = await this.boardsService.findBoardById(id);
+      res.json({
+        message: 'Board found successfully!',
+        body: { board },
+      });
+    } catch (err) {
+      if (err.message === 'Nothing was found') {
+        throw new BadRequestException('Nothing was found');
+      } else {
+        next(err);
+      }
+    }
   }
 
-  @Put(':board_id')
-  update(@Body() boardInput: UpdateBoardInput) {
-    return this.boardsService.updateBoardById(boardInput);
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() boardInput: UpdateBoardInput,
+    @Res() res: Response,
+    @Next() next: NextFunction,
+  ) {
+    try {
+      const board = await this.boardsService.updateBoardById(id, boardInput);
+      res.status(200).send({
+        message: 'Board Updated Successfully!',
+        body: { board },
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  @Delete(':board_id')
-  delete(@Param('board_id') board_id: number) {
-    return this.boardsService.deleteBoard(board_id);
+  @Delete(':id')
+  async delete(
+    @Param('id') id: number,
+    @Res() res: Response,
+    @Next() next: NextFunction,
+  ) {
+    try {
+      const board = await this.boardsService.deleteBoard(id);
+      res.status(200).send({
+        message: 'Board deleted successfully!',
+        body: { board },
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 }
