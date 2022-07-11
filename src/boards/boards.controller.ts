7f4dd8d1ffcf3ec8c0f9ba41a21a258079adb7ed
spankova <dshpankova282002@gmail.com>
2022-07-11 10:55:06 +0300

    feat: added put and delete metods for updating and deleting boards

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardInput } from './inputs/create-boards.input';
+import { UpdateBoardInput } from './inputs/update-board.input';
 
 @Controller('boards')
 export class BoardsController {
@@ -19,5 +28,15 @@ export class BoardsController {
   @Get('/:board_id')
   getById(@Param('board_id') board_id: number) {
     return this.boardsService.findBoardById(board_id);
+  }
+
+  @Put('/:board_id')
+  update(@Body() boardInput: UpdateBoardInput) {
+    return this.boardsService.updateBoardById(boardInput);
+  }
+
+  @Delete('/:board_id')
+  delete(@Param('board_id') board_id: number) {
+    return this.boardsService.deleteBoard(board_id);
   }
 }
