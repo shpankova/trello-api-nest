898fc7ea9bd3e5162dd03b68554dfc666703aae7
spankova <dshpankova282002@gmail.com>
2022-07-12 20:11:18 +0300

    refactor: changed db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
-import { BoardEntity } from './boards/entities/board.entity';
 import { CardsModule } from './cards/cards.module';
-
+import { typeOrmAsyncConfig } from './config/typeorm-config';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        type: config.get<'postgres'>('POSTGRES_CONNECTION'),
-        host: config.get<string>('POSTGRES_HOST'),
-        port: config.get<number>('POSTGRES_PORT'),
-        username: config.get<string>('POSTGRES_USER'),
-        password: config.get<string>('POSTGRES_PASSWORD'),
-        database: config.get<string>('POSTGRES_DB'),
-        entities: [BoardEntity],
-        synchronize: true,
-        autoLoadEntities: true,
-        logging: true,
-      }),
-    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     UsersModule,
     BoardsModule,
     CardsModule,
