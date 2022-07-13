95c5f48e1f4483c71dfb963f6bececf627c7c790
spankova <dshpankova282002@gmail.com>
2022-07-13 23:59:25 +0300

    feat: added create and update user inputs

---
+++ src/user/inputs/create-user.input.ts
@@ -0,0 +1,5 @@
+export class CreateUserInput {
+  username: string;
+  email: string;
+  password: string;
+}
---
+++ src/user/inputs/update-user.input.ts
@@ -0,0 +1,5 @@
+export class UpdateUserInput {
+  username: string;
+  email: string;
+  password: string;
+}
