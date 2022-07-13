352fccfa8e5e832653bb4429e42e9fe8a84dcef9
spankova <dshpankova282002@gmail.com>
2022-07-14 00:02:26 +0300

    feat: registration of jwt module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async () => ({
+        secret: 'jwt-secret-key',
+        signOptions: { expiresIn: '7d' },
+      }),
+    }),
+    PassportModule,
+    forwardRef(() => UserModule),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
