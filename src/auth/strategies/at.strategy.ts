1134260893bf7461fbb8037ae5f055ddbac99a50
spankova <dshpankova282002@gmail.com>
2022-07-14 21:38:02 +0300

    feat: create new passport strategy to access and refresh token

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -3,11 +3,10 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: true,
       secretOrKey: 'jwt-secret-key',
     });
   }
