e27097f174e60b0c47784f5956d47e82e9b44961
spankova <dshpankova282002@gmail.com>
2022-07-14 23:35:10 +0300

    feat: create auth dto

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsEmail, IsString } from 'class-validator';
+export class AuthDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  role: string;
+}
