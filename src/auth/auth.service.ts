7e31e28a8c45653ea331cdb9f7e96626956aec1f
spankova <dshpankova282002@gmail.com>
2022-07-14 23:33:14 +0300

    feat: updated auth service, added function to signin, signup, logout, refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,42 +1,106 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
-import { UserEntity } from 'src/user/entities/user.entity';
-import { UserService } from 'src/user/user.service';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { AuthDto } from './dto';
+import { Tokens } from './types';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @Inject(forwardRef(() => UserService))
-    private readonly userService: UserService,
-    private readonly jwtService: JwtService,
+    @InjectRepository(UserEntity)
+    private authRepository: Repository<UserEntity>,
+    private jwtService: JwtService,
   ) {}
 
-  generateJWT(userId: number, email: string, type: string) {
-    return this.jwtService.sign({
-      sub: userId,
+  async signup(dto: AuthDto): Promise<Tokens> {
+    const { email, password, role } = dto;
+    const hash = await this.hashData(password);
+    const newUser = await this.authRepository.save({
       email,
-      claim: type,
+      password: hash,
+      role,
     });
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
   }
 
-  hashPassword(password: string): Promise<string> {
-    return bcrypt.hash(password, 12);
-  }
-
-  comparePassword(newPassword: string, passwordHash: string): boolean {
-    return bcrypt.compareSync(newPassword, passwordHash);
-  }
-
-  async validateUser(email: string, password: string): Promise<UserEntity> {
-    try {
-      const findUser: UserEntity = await this.userService.findUserByEmail(
-        email,
-      );
-      if (!this.comparePassword(password, findUser.password)) return null;
-      return findUser;
-    } catch (err) {
-      console.log(err);
+  async signin(dto: AuthDto): Promise<Tokens> {
+    const { email, password } = dto;
+    const user = await this.authRepository.findOne({
+      where: { email },
+    });
+    if (!user) {
+      throw new ForbiddenException(`User with email ${email} is not found`);
     }
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (!passwordMatches) {
+      throw new ForbiddenException('Incorrect password');
+    }
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+    return tokens;
+  }
+
+  async logout(userId: number) {
+    await this.authRepository.update({ id: userId }, { hashedRt: null });
+  }
+
+  async refreshTokens(userId: number, rt: string) {
+    const user = await this.authRepository.findOne({
+      where: { id: userId },
+    });
+    if (!user) {
+      throw new ForbiddenException('Access denied');
+    }
+    const rtMatches = await bcrypt.compare(rt, user.hashedRt);
+    if (!rtMatches) {
+      throw new ForbiddenException('Access denied');
+    }
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+    return tokens;
+  }
+
+  async updateRtHash(userId: number, rt: string) {
+    const hash = await this.hashData(rt);
+    await this.authRepository.update({ id: userId }, { hashedRt: hash });
+  }
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'jwt-secret-key',
+          expiresIn: 60 * 15,
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'jwt-refresh-secret-key',
+          expiresIn: 60 * 60 * 24 * 7,
+        },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
   }
 }
