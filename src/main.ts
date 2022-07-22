1e17bc401179834b5122f248fe2ecb9c112f7009
spankova <dshpankova282002@gmail.com>
2022-07-22 10:53:01 +0300

    refactor: delete validation pipe and microservice

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   const config = await app.get(ConfigService);
   const PORT = config.get<number>('PORT');
   const HOST = config.get<string>('HOST');
@@ -14,14 +12,5 @@ async function bootstrap() {
   await app.listen(PORT, HOST);
 
   Logger.log(`Server started on ${await app.getUrl()}`);
-
-  await app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: {
-      port: 1025,
-    },
-  });
-
-  app.startAllMicroservices();
 }
 bootstrap();
